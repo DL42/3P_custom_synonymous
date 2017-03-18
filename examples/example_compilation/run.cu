c5bfa604810e6a9ef62d2cf45f90b4fce88869b9
David Lawrie <dlawrie@imac.attlocal.net>
2017-03-18 00:00:40 -0700

    improvements to example_compilation

--- examples/example_compilation/run.cu
+++ examples/example_compilation/run.cu
@@ -16,7 +16,6 @@ void run_migration_equilibrium_simulation(GO_Fish::allele_trajectories & a, floa
 	Sim_Model::F_mu_h_constant mutation(pow(10.f,-9)); 		//per-site mutation rate
 	Sim_Model::F_mu_h_constant inbreeding(1.f); 			//constant inbreeding
 	Sim_Model::demography_constant demography(1000); 		//number of individuals in both populations
-	std::cout<<migration_rate<<std::endl;
 	Sim_Model::migration_constant_equal migration(migration_rate,a.sim_input_constants.num_populations); //constant migration rate
 	Sim_Model::selection_constant selection(0.f); 			//constant selection coefficient
 	Sim_Model::F_mu_h_constant dominance(0.f); 				//constant allele dominance (ignored as population is fully inbred)
@@ -33,5 +32,5 @@ void run_migration_equilibrium_simulation(GO_Fish::allele_trajectories & a, floa
 	Sim_Model::bool_pulse<Sim_Model::bool_off,Sim_Model::bool_on> sample_strategy(dont_sample,sample,0,a.sim_input_constants.num_generations); //sample starting generation of second simulation (i.e. last generation of burn-in simulation)
 	a.sim_input_constants.num_generations = pow(10.f,3);	//scenario simulation 1,0000 generations
 	a.sim_input_constants.prev_sim_sample = 0; 				//start from previous simulation time sample 0
-	GO_Fish::run_sim(a,mutation,demography,migration,selection,inbreeding,dominance,dont_preserve,sample_strategy,c); //scenario simulation, start from migration equilibrium, sample both start and final generations
+	GO_Fish::run_sim(a,mutation,demography,migration,selection,inbreeding,dominance,dont_preserve,sample_strategy,a); //scenario simulation, start from migration equilibrium, sample both start and final generations
 }
