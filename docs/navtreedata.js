f44500819994a91aae1ee843e890b1a278f6dd2d
David Lawrie <dlawrie@imac.attlocal.net>
2017-03-19 14:58:59 -0700

    minor docs changes

--- docs/navtreedata.js
+++ docs/navtreedata.js
@@ -2,7 +2,6 @@ var NAVTREE =
 [
   [ "Parallel PopGen Package", "index.html", [
     [ "GO Fish v0.1", "index.html", null ],
-    [ "Test_page", "_test_page.html", null ],
     [ "Modules", "modules.html", "modules" ],
     [ "Namespaces", null, [
       [ "Namespace List", "namespaces.html", "namespaces" ],
@@ -28,8 +27,8 @@ var NAVTREE =
 
 var NAVTREEINDEX =
 [
-"_test_page.html",
-"struct_spectrum_1_1_s_f_s.html#a67e91ebc3cb47e80fc8509a0c1307d59"
+"annotated.html",
+"struct_spectrum_1_1_s_f_s.html#a68932f26bb024af3c72dd561dac35128"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
--- docs/navtreeindex1.js
+++ docs/navtreeindex1.js
@@ -1,9 +1,8 @@
 var NAVTREEINDEX1 =
 {
-"struct_spectrum_1_1_s_f_s.html#a67e91ebc3cb47e80fc8509a0c1307d59":[4,0,2,0,2],
-"struct_spectrum_1_1_s_f_s.html#a68932f26bb024af3c72dd561dac35128":[4,0,2,0,8],
-"struct_spectrum_1_1_s_f_s.html#ac04287f7d0f9df51f4a5a5efe5696307":[4,0,2,0,0],
-"struct_spectrum_1_1_s_f_s.html#ad84eaad4cfd4b3f060527ac4bb5c672a":[4,0,2,0,4],
-"struct_spectrum_1_1_s_f_s.html#aefb71eee25e91a21a81f092e5df55c5e":[4,0,2,0,7],
-"template__inline__simulation__functors_8cuh_source.html":[5,0,1,6]
+"struct_spectrum_1_1_s_f_s.html#a68932f26bb024af3c72dd561dac35128":[3,0,2,0,8],
+"struct_spectrum_1_1_s_f_s.html#ac04287f7d0f9df51f4a5a5efe5696307":[3,0,2,0,0],
+"struct_spectrum_1_1_s_f_s.html#ad84eaad4cfd4b3f060527ac4bb5c672a":[3,0,2,0,4],
+"struct_spectrum_1_1_s_f_s.html#aefb71eee25e91a21a81f092e5df55c5e":[3,0,2,0,7],
+"template__inline__simulation__functors_8cuh_source.html":[4,0,1,6]
 };
