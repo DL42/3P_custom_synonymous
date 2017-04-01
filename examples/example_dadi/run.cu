f7ee3c9aa924fd760011506f9394efd7e73f5c8c
David Lawrie <dlawrie@imac.attlocal.net>
2017-04-01 01:31:04 -0700

    added scale_factor for example_dadi

--- examples/example_dadi/run.cu
+++ examples/example_dadi/run.cu
@@ -19,68 +19,68 @@ void run_validation_test(){
 	typedef Sim_Model::migration_constant_directional<mig_const> mig_dir;
 	typedef Sim_Model::migration_constant_directional<mig_dir> mig_split;
 	typedef Sim_Model::migration_piecewise<mig_const,mig_split> split_pop0;
+	float scale_factor = 1.0f;											//entire simulation can be scaled up or down with little to no change in resulting normalized SFS
 
 	GO_Fish::allele_trajectories b;
-	b.sim_input_constants.num_populations = 2; 				//number of populations
-	b.sim_input_constants.num_generations = pow(10.f,3)+1;	//1,000 generations
+	b.sim_input_constants.num_populations = 2; 							//number of populations
+	b.sim_input_constants.num_generations = scale_factor*pow(10.f,3)+1;	//1,000 generations
 
-	Sim_Model::F_mu_h_constant codominant(0.5f); 			//dominance (co-dominant)
-	Sim_Model::F_mu_h_constant outbred(0.f); 				//inbreeding (outbred)
-	Sim_Model::F_mu_h_constant mutation(pow(10.f,-9)); 		//per-site mutation rate 10^-9
+	Sim_Model::F_mu_h_constant codominant(0.5f); 						//dominance (co-dominant)
+	Sim_Model::F_mu_h_constant outbred(0.f); 							//inbreeding (outbred)
+	Sim_Model::F_mu_h_constant mutation(pow(10.f,-9)/scale_factor); 	//per-site mutation rate 10^-9
 
-	int N_ind = pow(10.f,4)*(1+outbred(0,0));				//initial number of individuals in population, set to maintain consistent effective number of chromosomes across all inbreeding coefficients
+	int N_ind = scale_factor*pow(10.f,4)*(1+outbred(0,0));				//initial number of individuals in population, set to maintain consistent effective number of chromosomes across all inbreeding coefficients
 	dem_const pop0(N_ind);
 	dem_const pop1(0);
 	dem_pop_const gen0(pop0,pop1,1);
 	dem_const pop0_final(2*N_ind);
 	dem_pop_const gen1(pop0_final,pop1,1);
 	init_expansion gen_0_1(gen0,gen1,1);
-	exp_growth pop1_gen100((log(100.f)/(900.f)),0.05*N_ind,100);
+	exp_growth pop1_gen100((log(100.f)/(scale_factor*900.f)),0.05*N_ind,scale_factor*100);
 	dem_pop_const_exp gen100(pop0_final,pop1_gen100,1);
-	Sim_Model::demography_piecewise<init_expansion,dem_pop_const_exp> demography_model(gen_0_1,gen100,100);
+	Sim_Model::demography_piecewise<init_expansion,dem_pop_const_exp> demography_model(gen_0_1,gen100,scale_factor*100);
 	//for(int i = 0; i < 1000; i++){ std::cout<< demography_model(0,i) << "\t" << demography_model(1,i) << std::endl; }
 
 	mig_const no_mig_pop0;
 	mig_dir no_pop1_gen0(0.f,1,1,no_mig_pop0);
 	mig_split create_pop1(1.f,0,1,no_pop1_gen0);
-	split_pop0 migration_split(no_mig_pop0,create_pop1,100);
+	split_pop0 migration_split(no_mig_pop0,create_pop1,scale_factor*100);
 	float mig = 1.f/(2.f*N_ind);
-	mig_const mig_prop(mig,b.sim_input_constants.num_populations);	//migration rate
-	Sim_Model::migration_piecewise<split_pop0,mig_const> mig_model(migration_split,mig_prop,100+1);
+	mig_const mig_prop(mig,b.sim_input_constants.num_populations);		//migration rate
+	Sim_Model::migration_piecewise<split_pop0,mig_const> mig_model(migration_split,mig_prop,scale_factor*100+1);
 	//for(int i = 0; i < 1000; i++){ std::cout<< mig_model(0,0,i) << "\t" << mig_model(0,1,i) << "\t" << mig_model(1,0,i) << "\t" << mig_model(1,1,i) << std::endl; }
 
-	float gamma = -4*(1+outbred(0,0)); 								//effective selection //set to maintain consistent level of selection across all inbreeding coefficients for the same effective number of chromosomes, drift and selection are invariant with respect to inbreeding
+	float gamma = -4*(1+outbred(0,0)); 									//effective selection //set to maintain consistent level of selection across all inbreeding coefficients for the same effective number of chromosomes, drift and selection are invariant with respect to inbreeding
 	Sim_Model::selection_constant weak_del(gamma,demography_model,outbred);
 
-	b.sim_input_constants.compact_interval = 30;			//compact interval
-	b.sim_input_constants.num_sites = 100*2*pow(10.f,7); 	//number of sites
-	int sample_size = 1001;									//number of samples in SFS
+	b.sim_input_constants.compact_interval = 30;						//compact interval
+	b.sim_input_constants.num_sites = 100*2*pow(10.f,7); 				//number of sites
+	int sample_size = 1001;												//number of samples in SFS
 
-	int num_iter = 50;										//number of iterations
+	int num_iter = 50;													//number of iterations
     Spectrum::SFS my_spectra;
 
-    cudaEvent_t start, stop;								//CUDA timing functions
+    cudaEvent_t start, stop;											//CUDA timing functions
     float elapsedTime;
 	cudaEventCreate(&start);
 	cudaEventCreate(&stop);
 
 	float avg_num_mutations = 0;
 	float avg_num_mutations_sim = 0;
-	std::vector<std::vector<float> > results(num_iter); 	//storage for SFS results
+	std::vector<std::vector<float> > results(num_iter); 				//storage for SFS results
 	for(int j = 0; j < num_iter; j++){ results[j].reserve(sample_size); }
 
 	for(int j = 0; j < num_iter; j++){
-		if(j == num_iter/2){ cudaEventRecord(start, 0); } 	//use 2nd half of the simulations to time simulation runs + SFS creation
+		if(j == num_iter/2){ cudaEventRecord(start, 0); } 				//use 2nd half of the simulations to time simulation runs + SFS creation
 
-		b.sim_input_constants.seed1 = 0xbeeff00d + 2*j; 	//random number seeds
+		b.sim_input_constants.seed1 = 0xbeeff00d + 2*j; 				//random number seeds
 		b.sim_input_constants.seed2 = 0xdecafbad - 2*j;
 		GO_Fish::run_sim(b, mutation, demography_model, mig_model, weak_del, outbred, codominant, Sim_Model::bool_off(), Sim_Model::bool_off());
 		Spectrum::site_frequency_spectrum(my_spectra,b,0,1,sample_size);
 
 		avg_num_mutations += ((float)my_spectra.num_mutations)/num_iter;
 		avg_num_mutations_sim += b.maximal_num_mutations()/num_iter;
-		for(int i = 0; i < sample_size; i++){ results[j][i] = my_spectra.frequency_spectrum[i]; /*if(i == 1){ std::cout<< results[j][i]/(b.num_sites() - results[j][0]) << std::endl; } */}
-		//std::cout<<std::endl;
+		for(int i = 0; i < sample_size; i++){ results[j][i] = my_spectra.frequency_spectrum[i]; }
 	}
 
 	elapsedTime = 0;
