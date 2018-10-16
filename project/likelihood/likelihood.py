ff8cfd6cb0ffa708a992a68ab070b7442d93d315
David Lawrie <>
2018-10-16 06:35:48 -0700

    clean up likelihood.py

--- project/likelihood/likelihood.py
+++ project/likelihood/likelihood.py
@@ -13,12 +13,7 @@ alpha = np.array([1]*79,dtype=np.float32)
 theta = 0.01
 inbreeding = 0.0
 num_sites = 2*pow(10,7)
-before = time.time()
 selected = mse_gpu.calc_sfs_mse(gamma,dominance,inbreeding,proportion,theta,alpha,mse)
-process_time = time.time() - before
-print(process_time)
-print(selected*num_sites)
-print(sum(selected))
 
 def theor_neutral(theta, alpha, mse):
 	gamma = np.array([0],dtype=np.float32)
@@ -27,20 +22,7 @@ def theor_neutral(theta, alpha, mse):
 	inbreeding = 0
 	return mse_gpu.calc_sfs_mse(gamma,dominance,inbreeding,proportion,theta,alpha,mse)
 
-before = time.time()
 neutral = theor_neutral(theta, alpha, mse)
-process_time = time.time() - before
-print(process_time)
-print(neutral*num_sites)
-print(sum(neutral))
-
-before = time.time()
-alpha = np.array([0.9]*79,dtype=np.float32)
-renorm = mse_gpu.renormalize_SFS(neutral, alpha, mse)
-process_time = time.time() - before
-print(process_time)
-print(renorm*num_sites)
-print(sum(renorm))
 
 # num_test_sites=sum(obs_test_sfs)
 # num_ref_sites=sum(obs_ref_sfs)
@@ -89,27 +71,18 @@ def total_likelihood(obs_test_sfs, obs_ref_sfs, theta, gamma_array, d_array, inb
    
     return loglambda,alpha,theor_test_sfs,theor_ref_sfs
 
-neu = neutral*num_sites
-sel = selected*num_sites
-before = time.time()
-start_index = 0
-if (not(zero_class)):
-    start_index = 1
-    
-r = range(start_index,neu.size)
-loglambda_neu = np.dot(neu[r],np.log(neutral[r]))
-loglambda_sel = np.dot(sel[r],np.log(selected[r]))
-loglambda = loglambda_neu + loglambda_sel
-process_time = time.time() - before
-print("WTF",process_time,loglambda)
-
 alpha = np.array([1]*79,dtype=np.float32)
 boundary_array = np.array([[0.0,0.0],[0.0,-500]])
 p_array = np.array([0.85])
 sel = selected*num_sites
 neu = neutral*num_sites
+lik = total_likelihood(sel, neu, 0.01, gamma, dominance, inbreeding, p_array, boundary_array, alpha, 0, mse)
 before = time.time()
 lik = total_likelihood(sel, neu, 0.01, gamma, dominance, inbreeding, p_array, boundary_array, alpha, 0, mse)
 process_time = time.time() - before
 print(process_time)
 print(lik[0])
+
+if __name__ == '__main__':
+    pass
+    
