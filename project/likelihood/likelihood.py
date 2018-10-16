a32e2128460d9784bd4988c1379a8b70e4eec103
David Lawrie <>
2018-10-16 05:46:38 -0700

    likelihood function works (tested against Matlab code)
    oddly: for python’s numpy functions - at least when compiled against openBLAS from conda-forge - the first time the dot product is run, there is a small, but measurable setup, for small arrays, much longer than the time it takes to calculate the dot product
    that's okay since all the subsequent runs will be so fast

--- project/likelihood/likelihood.py
+++ project/likelihood/likelihood.py
@@ -1,132 +1,115 @@
 import numpy as np
 import mse_gpu
 import time
-fold = False
+from math import fsum
+
+fold = True
 zero_class = True
 mse = mse_gpu.MSE(160,40000,fold,zero_class)
 gamma = np.array([0,-200],dtype=np.float32)
-dominance = np.array([0,0],dtype=np.float32) #effectively ignored since inbreeding is set to 1
+dominance = np.array([0.5,0.5],dtype=np.float32) #effectively ignored since inbreeding is set to 1
 proportion = np.array([0.85,0.15],dtype=np.float32)
-alpha = np.array([1]*158,dtype=np.float32)
+alpha = np.array([1]*79,dtype=np.float32)
 theta = 0.01
-inbreeding = 1.0
+inbreeding = 0.0
 num_sites = 2*pow(10,7)
 before = time.time()
-result = mse_gpu.calc_sfs_mse(gamma,dominance,inbreeding,proportion,theta,alpha,mse)
+selected = mse_gpu.calc_sfs_mse(gamma,dominance,inbreeding,proportion,theta,alpha,mse)
 process_time = time.time() - before
 print(process_time)
-print(result*num_sites)
-print(sum(result))
+print(selected*num_sites)
+print(sum(selected))
 
-def theor_neutral(mse, theta):
+def theor_neutral(theta, alpha, mse):
 	gamma = np.array([0],dtype=np.float32)
-	dominance = np.array([0],dtype=np.float32) #effectively ignored since inbreeding is set to 1
+	dominance = np.array([0.5],dtype=np.float32) #effectively ignored since inbreeding is set to 1
 	proportion = np.array([1],dtype=np.float32)
-	alpha = np.array([1]*158,dtype=np.float32)
-	inbreeding = 1
+	inbreeding = 0
 	return mse_gpu.calc_sfs_mse(gamma,dominance,inbreeding,proportion,theta,alpha,mse)
 
 before = time.time()
-result = theor_neutral(mse, theta)
+neutral = theor_neutral(theta, alpha, mse)
 process_time = time.time() - before
 print(process_time)
-print(result*num_sites)
-print(sum(result))
+print(neutral*num_sites)
+print(sum(neutral))
 
 before = time.time()
-alpha = np.array([0.9]*158,dtype=np.float32)
-result = mse_gpu.renormalize_SFS(result, alpha, mse)
+alpha = np.array([0.9]*79,dtype=np.float32)
+renorm = mse_gpu.renormalize_SFS(neutral, alpha, mse)
 process_time = time.time() - before
 print(process_time)
-print(result*num_sites)
-print(sum(result))
+print(renorm*num_sites)
+print(sum(renorm))
+
 # num_test_sites=sum(obs_test_sfs)
 # num_ref_sites=sum(obs_ref_sfs)
-# n_samp = length(obs_test_sfs) - 1
-# if(fold): n_samp = 2*(length(obs_test_sfs) - 1)
-# 
+# n_samp = length(obs_test_sfs) - 1 
+# dominance = np.array([0.5]*proportion.size,dtype=np.float32)
+# inbreeding = 0.0
+ 
+def total_likelihood(obs_test_sfs, obs_ref_sfs, theta, gamma_array, d_array, inbreeding, p_array, boundary_array, alpha, lethal_perc, mse):
+    p_array=abs(p_array)
+    alpha=abs(alpha)
+    theta=abs(theta)
+    if (gamma_array.size == 1):
+        p_array=np.array([],dtype=np.float32)
+    
+    sum_p_array = fsum(p_array)
+    loglambda = float("-inf")
+    
+    if ((sum_p_array > (1 - lethal_perc)) or (lethal_perc > 1) or (lethal_perc < 0)):
+        return loglambda,alpha,None,None
+    
+    bound_check=0
+    for gamma, boundary in zip(gamma_array, boundary_array):
+        if (boundary[0] == boundary[1]):
+            gamma = boundary[0]
+        else:
+            bound_check += (abs(gamma) > abs(boundary[1])) + (abs(gamma) < abs(boundary[0]))
+    
+    if (bound_check > 0):
+        return loglambda,alpha,None,None
 
-# 
-# def total_likelihood(obs_test_sfs, obs_ref_sfs, n_samp, unnorm_theor_ref_sfs, mse, theta, gamma_array, p_array, boundary_array, alpha, lethal_perc):
-#     p_array=abs(p_array)
-#     alpha=abs(alpha)
-#     theta=abs(theta)
-#     if (length(gamma_array) == 1):
-#         p_array=np.array([],dtype=np.float32)
-#     
-#     if ((sum(p_array) > (1 - lethal_perc)) or (lethal_perc > 1) or (lethal_perc < 0)):
-#         loglambda=- 1 / 0
-#         return loglambda,alpha,None,None
-#     
-#     bool=0
-#     for index1 in arange(1,length(gamma)).reshape(-1):
-#         if (boundary_array(index1,2) == boundary_array(index1,1)):
-#             gamma_array[index1]=boundary_array(index1,2)
-#         else:
-#             bool=bool + (gamma_array(index1) < boundary_array(index1,2)) + (gamma_array(index1) > boundary_array(index1,1))
-#     
-#     if (bool > 0):
-#         # bool,gamma_array
-#         loglambda=- 1 / 0
-#         return loglambda,alpha,None,None
-#     
-#     mu_persite=theta / (dot(4,N_pop))
-#     g_mix_neu=theor_SFS(concat([0,1.0]),mu_persite,num_ref_sites,N_pop,n_samp,P_samp)
-#     
-#     theor_ref_sfs=zeros((length(obs_ref_sfs) - 1),1)
-#     theor_ref_sfs[1]=dot(1,(g_mix_neu(1) + g_mix_neu(n_samp - 1)))
-#     for k in arange(2,(length(obs_ref_sfs) - 1)).reshape(-1):
-#         theor_ref_sfs[k]=dot(alpha((k - 1)),(g_mix_neu(k) + g_mix_neu(n_samp - k)))
-#         if (k == n_samp - k):
-#             theor_ref_sfs[k]=dot(alpha((k - 1)),g_mix_neu(k))
-#     
-#     
-#     if (zero_class):
-#         #can't use m_samp because alphas, so re-sum number of neutral SNPs
-#         g0_mix_neu=num_ref_sites - sum(theor_ref_sfs)
-#         theor_ref_sfs=concat([[g0_mix_neu],[theor_ref_sfs]])
-#         rho_mix_neu_fold=theor_ref_sfs / num_ref_sites
-#     else:
-#         rho_mix_neu=theor_ref_sfs / sum(theor_ref_sfs)
-#         rho_mix_neu_fold=concat([[0],[rho_mix_neu]])
-#     
-#     
-#     gamma_array_comb=concat([[gamma_array],[concat([p_array,(1 - lethal_perc) - sum(p_array)])]]).T
-#     g_mix_sel=theor_SFS(gamma_array_comb,mu_persite,num_test_sites,N_pop,n_samp,P_samp)
-# 
-#     theor_test_sfs=zeros((length(obs_ref_sfs) - 1),1)
-#     theor_test_sfs[1]=dot(1,(g_mix_sel(1) + g_mix_sel(n_samp - 1)))
-#     for k in arange(2,(length(obs_ref_sfs) - 1)).reshape(-1):
-#         theor_test_sfs[k]=dot(alpha((k - 1)),(g_mix_sel(k) + g_mix_sel(n_samp - k)))
-#         if (k == n_samp - k):
-#             theor_test_sfs[k]=dot(alpha((k - 1)),g_mix_sel(k))
-#     
-#     
-#     if (zero_class):
-#         #can't use m_samp because alphas, so re-sum number of selected SNPs
-#         g0_mix_sel=num_test_sites - sum(theor_test_sfs)
-#         theor_test_sfs=concat([[g0_mix_sel],[theor_test_sfs]])
-#         rho_mix_sel_fold=theor_test_sfs / num_test_sites
-#     else:
-#         rho_mix_sel=theor_test_sfs / sum(theor_test_sfs)
-#         rho_mix_sel_fold=concat([[0],[rho_mix_sel]])
-#     
-#     
-#     loglambda_neu=0
-#     loglambda_sel=0
-#     if (zero_class):
-#         loglambda_neu=dot(obs_ref_sfs(1),log(rho_mix_neu_fold(1)))
-#         loglambda_sel=dot(obs_test_sfs(1),log(rho_mix_sel_fold(1)))
-#     
-#     
-#     
-#     for k in arange(2,length(obs_ref_sfs)).reshape(-1):
-#         loglambda_neu=loglambda_neu + dot(obs_ref_sfs(k),log(rho_mix_neu_fold(k)))
-#     
-#     
-#     for k in arange(2,length(obs_test_sfs)).reshape(-1):
-#         loglambda_sel=loglambda_sel + dot(obs_test_sfs(k),log(rho_mix_sel_fold(k)))
-#     
-#     
-#     loglambda=loglambda_neu + loglambda_sel
-#     return loglambda,alpha,theor_test_sfs,theor_ref_sfs
+    theor_ref_sfs = theor_neutral(theta, alpha, mse)
+    
+    proportion = np.append(p_array,[(1 - lethal_perc) - sum_p_array])
+    theor_test_sfs = mse_gpu.calc_sfs_mse(gamma_array,d_array,inbreeding,proportion,theta,alpha,mse)
+    
+    start_index = 0
+    if (not(zero_class)):
+    	start_index = 1
+    
+    r = range(start_index,obs_ref_sfs.size)	
+    
+    loglambda_neu = np.dot(obs_ref_sfs[r],np.log(theor_ref_sfs[r]))
+    loglambda_sel = np.dot(obs_test_sfs[r],np.log(theor_test_sfs[r]))
+
+    loglambda = loglambda_neu + loglambda_sel
+   
+    return loglambda,alpha,theor_test_sfs,theor_ref_sfs
+
+neu = neutral*num_sites
+sel = selected*num_sites
+before = time.time()
+start_index = 0
+if (not(zero_class)):
+    start_index = 1
+    
+r = range(start_index,neu.size)
+loglambda_neu = np.dot(neu[r],np.log(neutral[r]))
+loglambda_sel = np.dot(sel[r],np.log(selected[r]))
+loglambda = loglambda_neu + loglambda_sel
+process_time = time.time() - before
+print("WTF",process_time,loglambda)
+
+alpha = np.array([1]*79,dtype=np.float32)
+boundary_array = np.array([[0.0,0.0],[0.0,-500]])
+p_array = np.array([0.85])
+sel = selected*num_sites
+neu = neutral*num_sites
+before = time.time()
+lik = total_likelihood(sel, neu, 0.01, gamma, dominance, inbreeding, p_array, boundary_array, alpha, 0, mse)
+process_time = time.time() - before
+print(process_time)
+print(lik[0])
