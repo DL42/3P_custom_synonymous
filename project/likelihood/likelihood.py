b30c61d81c30640ccb4e98626cda1c03a909628a
Unknown <dlawrie@alumni.stanford.edu>
2018-10-17 19:33:35 -0700

    added maximum-likelihood scenarios
    fixed whitespace

--- project/likelihood/likelihood.py
+++ project/likelihood/likelihood.py
@@ -31,32 +31,32 @@ neutral = theor_neutral(theta, alpha, mse)
 # inbreeding = 0.0
  
 def total_likelihood(obs_test_sfs, obs_ref_sfs, theta, gamma_array, d_array, inbreeding, p_array, boundary_array, alpha, lethal_perc, mse):
-    p_array=abs(p_array)
-    alpha=abs(alpha)
-    theta=abs(theta)
-    if (gamma_array.size == 1):
-        p_array=np.array([],dtype=np.float32)
+	p_array=abs(p_array)
+	alpha=abs(alpha)
+	theta=abs(theta)
+	if (gamma_array.size == 1):
+		p_array=np.array([],dtype=np.float32)
     
-    sum_p_array = fsum(p_array)
-    loglambda = float("-inf")
+	sum_p_array = fsum(p_array)
+	loglambda = float("-inf")
     
-    if ((sum_p_array > (1 - lethal_perc)) or (lethal_perc > 1) or (lethal_perc < 0)):
-        return loglambda,alpha,None,None
+	if ((sum_p_array > (1 - lethal_perc)) or (lethal_perc > 1) or (lethal_perc < 0)):
+		return loglambda,alpha,None,None
     
-    bound_check=0
-    for gamma, boundary in zip(gamma_array, boundary_array):
-        if (boundary[0] == boundary[1]):
+	bound_check=0
+	for gamma, boundary in zip(gamma_array, boundary_array):
+		if (boundary[0] == boundary[1]):
             gamma = boundary[0]
-        else:
+		else:
             bound_check += (abs(gamma) > abs(boundary[1])) + (abs(gamma) < abs(boundary[0]))
     
-    if (bound_check > 0):
-        return loglambda,alpha,None,None
+    	if (bound_check > 0):
+        	return loglambda,alpha,None,None
 
-    theor_ref_sfs = theor_neutral(theta, alpha, mse)
+    	theor_ref_sfs = theor_neutral(theta, alpha, mse)
     
-    proportion = np.append(p_array,[(1 - lethal_perc) - sum_p_array])
-    theor_test_sfs = mse_gpu.calc_sfs_mse(gamma_array,d_array,inbreeding,proportion,theta,alpha,mse)
+    	proportion = np.append(p_array,[(1 - lethal_perc) - sum_p_array])
+    	theor_test_sfs = mse_gpu.calc_sfs_mse(gamma_array,d_array,inbreeding,proportion,theta,alpha,mse)
     
     start_index = 0
     if (not(zero_class)):
@@ -64,12 +64,12 @@ def total_likelihood(obs_test_sfs, obs_ref_sfs, theta, gamma_array, d_array, inb
     
     r = range(start_index,obs_ref_sfs.size)	
     
-    loglambda_neu = np.dot(obs_ref_sfs[r],np.log(theor_ref_sfs[r]))
-    loglambda_sel = np.dot(obs_test_sfs[r],np.log(theor_test_sfs[r]))
+	loglambda_neu = np.dot(obs_ref_sfs[r],np.log(theor_ref_sfs[r]))
+	loglambda_sel = np.dot(obs_test_sfs[r],np.log(theor_test_sfs[r]))
 
-    loglambda = loglambda_neu + loglambda_sel
+	loglambda = loglambda_neu + loglambda_sel
    
-    return loglambda,alpha,theor_test_sfs,theor_ref_sfs
+	return loglambda,alpha,theor_test_sfs,theor_ref_sfs
 
 alpha = np.array([1]*79,dtype=np.float32)
 boundary_array = np.array([[0.0,0.0],[0.0,-500]])
@@ -110,70 +110,134 @@ def expand_alpha(alpha_in, SFS_size, alpha_pattern):
 SFS_size = 81
 alpha_pattern = np.array([[0,1],[1,3],[3,7],[7,15],[15,31],[31,79]])
 check_alpha_pattern(SFS_size, alpha_pattern)
-alpha = expand_alpha(alpha_in, SFS_size, alpha_pattern)
+my_expand_alpha = lambda x: expand_alpha(x, SFS_size, alpha_pattern)
+ap_size = alpha_in.size
+p_size = initial_p_array.size
+alpha = my_expand_alpha(alpha_in)
 print(alpha)
 
-def output_results(file_name):
+def re_param_neutral_alpha(x):
+	theta_site = x[ap_size]
+	p_array = []
+	alpha = my_expand_alpha(x[0:ap_size])
+	g_array = np.array([0],dtype=np.float32)
+	d_array = np.array([0.5],dtype=np.float32)
+	b_array = np.array([0,0])
+	loglambda = -1*total_likelihood(obs_ref_sfs, obs_ref_sfs, theta_site, g_array, d_array, inbreeding, p_array, b_array, alpha, 0, mse)
+	return loglambda
+    
+def re_param(x):
+	theta_site = x[ap_size]
+	p_array = x[(ap_size+1):(ap_size+1+p_size)]
+	if (free_alpha and not(neutral_alpha)):
+		alpha = my_expand_alpha(x[0:ap_size])
+	else:
+		alpha = my_expand_alpha(fixed_alpha[0:ap_size])
+	g_array = x[ap_size+1+p_size:x.size]
+	d_array = np.array([0.5]*g_array.size,dtype=np.float32)
+	loglambda = -1*total_likelihood(obs_test_sfs, obs_ref_sfs, theta_site, g_array, d_array, inbreeding, p_array, boundary_array, alpha, 0, mse)
+	return loglambda
+    
+def re_param_neu(x):
+	theta_site = x[ap_size]
+	p_array = []
+	if (free_alpha and not(neutral_alpha)):
+		alpha = my_expand_alpha(x[0:ap_size])
+	else:
+    	alpha = my_expand_alpha(fixed_alpha[0:ap_size])
+	g_array = np.array([0],dtype=np.float32)
+	d_array = np.array([0.5],dtype=np.float32)
+	b_array = np.array([0,0])
+	loglambda = -1*total_likelihood(obs_test_sfs, obs_ref_sfs, theta_site, g_array, d_array, inbreeding, p_array, b_array, alpha, 0, mse)
+	return loglambda
+    
+def re_param_neu_lethal(x):
+	theta_site = x[ap_size]
+	p_array = 1.0 - x[x.size-1]
+	if (free_alpha and not(neutral_alpha)):
+		alpha = my_expand_alpha(x[0:ap_size])
+	else:
+		alpha = my_expand_alpha(fixed_alpha[0:ap_size])
+	g_array = np.array([0],dtype=np.float32)
+	d_array = np.array([0.5],dtype=np.float32)
+	b_array = np.array([0,0])
+	lethal_perc = x[x.size-1]
+	loglambda = -1*total_likelihood(obs_test_sfs, obs_ref_sfs, theta_site, g_array, d_array, inbreeding, p_array, b_array, alpha, lethal_perc, mse)
+	return loglambda
+    
+def re_param_lethal(x):
+	theta_site = x[ap_size]
+	p_array=x[(ap_size+1):(ap_size+1+p_size)]
+	if (free_alpha and not(neutral_alpha)):
+		alpha = my_expand_alpha(x[0:ap_size])
+	else:
+		alpha = my_expand_alpha(fixed_alpha[0:ap_size])
+	g_array = x[ap_size+1+p_size:x.size]
+	d_array = np.array([0.5]*g_array.size,dtype=np.float32)
+	lethal_perc = x[x.size-1]
+	loglambda = -1*total_likelihood(obs_test_sfs, obs_ref_sfs, theta_site, g_array, d_array, inbreeding, p_array, boundary_array, alpha, lethal_perc, mse)
+	return loglambda
 
-    f = open(file_name,"a")
-    f.write('{}_selection_classes\t'.format(initial_gamma_array.size))
-    if (zero_class):
-        f.write('zero_class\t')
-    else:
-        f.write('shape_only\t')
+def output_results(file_name):
+	f = open(file_name,"a")
+	f.write('{}_selection_classes\t'.format(initial_gamma_array.size))
+	if (zero_class):
+		f.write('zero_class\t')
+	else:
+		f.write('shape_only\t')
     
-    if (free_alpha and not(neutral_alpha)):
-        f.write('freeAlpha6Bins\t') #add alpha_pattern to description
-    else:
-        if (neutral_alpha and free_alpha):
-            f.write('neutAlpha6Bins\t')
-        else:
-            f.write('fixedAlpha_one\t')
+	if (free_alpha and not(neutral_alpha)):
+		f.write('freeAlpha6Bins\t') #add alpha_pattern to description
+	else:
+		if (neutral_alpha and free_alpha):
+			f.write('neutAlpha6Bins\t')
+		else:
+			f.write('fixedAlpha_one\t')
     
-    f.write('({}'.format(initial_gamma_array[0]))
-    for gamma in initial_gamma_array[1:]:
-        f.write(',{}'.format(gamma))
+	f.write('({}'.format(initial_gamma_array[0]))
+	for gamma in initial_gamma_array[1:]:
+		f.write(',{}'.format(gamma))
     
-    f.write(';{}'.format(boundary_array[0]))
-    for boundary in boundary_array[1:]:
-        f.write(',{}'.format(boundary))
+	f.write(';{}'.format(boundary_array[0]))
+	for boundary in boundary_array[1:]:
+		f.write(',{}'.format(boundary))
     
-    f.write(';{}'.format(initial_p_array[0]))
-    for prop in initial_p_array[1:]:
-        f.write(',{}'.format(prop))   
-    f.write('{};'.format((1 - fsum(initial_p_array))))
+	f.write(';{}'.format(initial_p_array[0]))
+	for prop in initial_p_array[1:]:
+		f.write(',{}'.format(prop))   
+	f.write('{};'.format((1 - fsum(initial_p_array))))
     
-    f.write('{:6.4f};'.format(initial_theta_site))
-    f.write('{},{},{})\t('.format(initial_lethal_array[0],initial_lethal_array[1],N_chrome))
+	f.write('{:6.4f};'.format(initial_theta_site))
+	f.write('{},{},{})\t('.format(initial_lethal_array[0],initial_lethal_array[1],N_chrome))
     
-    stop = max_gamma.size - 1
-    pfinal = 1
-    if(stop != 0):
-    	pfinal = 1 - fsum(max_p_array)
-    	for gamma, boundary, p in zip(max_gamma[:stop], boundary_array[:stop], max_p_array):
-        	if (boundary[0] == boundary[1]):
-            	gamma = boundary[0]
-        	f.write('{ } {:6.4f}, '.format(gamma,p))
+	stop = max_gamma.size - 1
+	pfinal = 1
+	if(stop != 0):
+		pfinal = 1 - fsum(max_p_array)
+		for gamma, boundary, p in zip(max_gamma[:stop], boundary_array[:stop], max_p_array):
+			if (boundary[0] == boundary[1]):
+				gamma = boundary[0]
+			f.write('{ } {:6.4f}, '.format(gamma,p))
     	
-    if (boundary_array[stop,0] == boundary_array[stop,1]):
-        max_gamma[stop] = boundary_array[stop,0]
+	if (boundary_array[stop,0] == boundary_array[stop,1]):
+		max_gamma[stop] = boundary_array[stop,0]
     
-    if (zero_class):
-        f.write('{ } {:6.4f}, theta {:6.4f}): '.format(max_gamma[stop],pfinal,max_theta_site))
-    else:
-        f.write('{ } {:6.4f}): '.format(max_gamma[stop],pfinal))
+	if (zero_class):
+		f.write('{ } {:6.4f}, theta {:6.4f}): '.format(max_gamma[stop],pfinal,max_theta_site))
+	else:
+		f.write('{ } {:6.4f}): '.format(max_gamma[stop],pfinal))
     
-    f.write('{:6.4f}\t'.format(max_likelihood))
-    if (zero_class):
-        f.write('(neutral 1.0, theta {:6.4f}): {:6.4f}\t'.format(max_theta_site_neu,all_neutral_lik))
-    else:
-        f.write('(neutral 1.0): {:6.4f}\t'.format(all_neutral_lik))
+	f.write('{:6.4f}\t'.format(max_likelihood))
+	if (zero_class):
+		f.write('(neutral 1.0, theta {:6.4f}): {:6.4f}\t'.format(max_theta_site_neu,all_neutral_lik))
+	else:
+	f.write('(neutral 1.0): {:6.4f}\t'.format(all_neutral_lik))
     
     
-    if (zero_class):
-        f.write('(neutral {:6.4f}, lethal {:6.4f}, theta {:6.4f}): {:6.4f}\t('.format((1 - max_neu_lethal_perc),max_neu_lethal_perc,max_theta_site_neu_lethal,max_neutral_lethal_lik))
-        pfinal = 1 - max_lethal_perc
-        if(stop != 0):
+	if (zero_class):
+		f.write('(neutral {:6.4f}, lethal {:6.4f}, theta {:6.4f}): {:6.4f}\t('.format((1 - max_neu_lethal_perc),max_neu_lethal_perc,max_theta_site_neu_lethal,max_neutral_lethal_lik))
+		pfinal = 1 - max_lethal_perc
+		if(stop != 0):
 			pfinal = 1 - fsum(max_lethal_p_array) - max_lethal_perc	
 			for lethal_gamma, boundary, lethal_p in zip(max_lethal_gamma[:stop], boundary_array[:stop], max_lethal_p_array):
 				if (boundary_array[index,0] == boundary_array[index,1]):
@@ -182,10 +246,10 @@ def output_results(file_name):
 			if (boundary_array[stop,0] == boundary_array[stop,1]):
 				max_lethal_gamma[stop]=boundary_array[stop,0]
 							
-        f.write('{ } {:6.4f}, lethal {:6.4f}, theta {:6.4f}): {:6.4f}'.format(max_lethal_gamma(stop),pfinal,max_lethal_perc,max_theta_site_lethal,max_lethal_lik))
+		f.write('{ } {:6.4f}, lethal {:6.4f}, theta {:6.4f}): {:6.4f}'.format(max_lethal_gamma(stop),pfinal,max_lethal_perc,max_theta_site_lethal,max_lethal_lik))
     
-    f.write('\n')
-    fclose(fileID)
+	f.write('\n')
+	fclose(fileID)
 
 if __name__ == '__main__':
     pass
